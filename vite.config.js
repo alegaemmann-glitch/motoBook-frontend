b5877ff574e3b3a7f5362c929a92e7f0a6dc3ef6
ziandrel <jedrafael0033@gmail.com>
2025-10-17 02:06:46 +0800

    fix undefined react error

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/",
   preview: {
     allowedHosts: ["moto-book-frontend-production.up.railway.app"],
   },
+  plugins: [react()],
 });
