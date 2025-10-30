725776b4713a40a1eabfc5eb9ec92dc5f0a0437f
alegaemmann-glitch <alegaemmann@gmail.com>
2025-10-30 10:44:07 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/",
   preview: {
-    allowedHosts: ["moto-book-frontend-production.up.railway.app"],
+    allowedHosts: ["motobook.up.railway.app"],
   },
   plugins: [react()],
 });
